16798f44bf1bd143aec40c16213edccc58f22f45
tawoe <tobias@tesobe.com>
2018-11-30 07:59:10 +0100

    add kafka adapter

---
+++ src/test/scala/Mapper.scala
@@ -0,0 +1,8 @@
+import org.scalatest.{FunSuite, Matchers}
+import net.liftweb
+
+class Mapper extends FunSuite  with Matchers{
+
+  //TODO: Mapper
+
+}
--- src/test/scala/PromisesAndFuture.scala
+++ src/test/scala/PromisesAndFuture.scala
@@ -3,5 +3,5 @@ import org.scalatest.{FunSuite, Matchers}
 class PromisesAndFuture extends FunSuite  with Matchers{
   
   //TODO: Promises and Futures in Scala and akka
-
+  
 }
